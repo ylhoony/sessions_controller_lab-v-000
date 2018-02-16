4ac4ee17a9099b299eca422e999a7602577fa6fc
Hoon Lee <yl.hoony@gmail.com>
2018-02-16 21:43:08 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    if params[:name].present?
+      session[:name] = params[:name]
+      redirect_to "/"
+    else
+      redirect_to "/login"
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to "/login"
+  end
+
+end
